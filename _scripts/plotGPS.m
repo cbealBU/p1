d1f18e4c68d815c2bb80259ddcee9c066ea565bd
Craig Beal <cbeal@bucknell.edu>
2023-12-18 13:11:30 -0500

    Fix: Updated sample times in MPU model. May still have issues due to modified serial library

--- _scripts/plotGPS.m
+++ _scripts/plotGPS.m
@@ -1,20 +1,22 @@
 % GPS plots only
 
-GPS_ToW = rt_GPS(:,1);
-GPS_Week = rt_GPS(:,2);
-GPS_Sats = rt_GPS(:,3);
-GPS_Mode = rt_GPS(:,4);
-GPS_Lat = rt_GPS(:,5);
-GPS_Long = rt_GPS(:,6);
-GPS_Alt = rt_GPS(:,7);
-GPS_HorSpd = rt_GPS(:,8);
-GPS_VrtSpd = rt_GPS(:,9);
-GPS_CoG = rt_GPS(:,10);
-GPS_Hdg = rt_GPS(:,11);
-GPS_Roll = rt_GPS(:,12);
-GPS_AttStat = [double(bitand(uint16(rt_GPS(:,14)),uint16(15))),...
-    double(bitshift(bitand(uint16(rt_GPS(:,14)),uint16(240)),-4))+0.04,...
-    double(bitshift(bitand(uint16(rt_GPS(:,14)),uint16(3840)),-8))+0.08];
+inds = find(rt_GPS(:,4) >= 4);
+
+GPS_ToW = rt_GPS(inds,1);
+GPS_Week = rt_GPS(inds,2);
+GPS_Sats = rt_GPS(inds,3);
+GPS_Mode = rt_GPS(inds,4);
+GPS_Lat = rt_GPS(inds,5);
+GPS_Long = rt_GPS(inds,6);
+GPS_Alt = rt_GPS(inds,7);
+GPS_HorSpd = rt_GPS(inds,8);
+GPS_VrtSpd = rt_GPS(inds,9);
+GPS_CoG = rt_GPS(inds,10);
+GPS_Hdg = rt_GPS(inds,11);
+GPS_Roll = rt_GPS(inds,12);
+GPS_AttStat = [double(bitand(uint16(rt_GPS(inds,14)),uint16(15))),...
+    double(bitshift(bitand(uint16(rt_GPS(inds,14)),uint16(240)),-4))+0.04,...
+    double(bitshift(bitand(uint16(rt_GPS(inds,14)),uint16(3840)),-8))+0.08];
 
 figure;
 subplot(3,6,1)
